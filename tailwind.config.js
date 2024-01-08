d6ffe3c51b869710275719adf10fce21ed6a5f7d
Aditya <90451018+realAdif@users.noreply.github.com>
2024-01-08 16:41:59 +1100

    CSS setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-text': '#3b4363',
+        'score-text': '#2a46c0',
+        'header-outline': '#606e85',
+      },
+      fontFamily: {
+        sans: ['Barlow Semi Condensed'],
+      },
+    },
   },
   plugins: [],
 };
