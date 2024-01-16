5be63b1a7b4622d40c8a384bc01a5424a5eee518
Aditya <90451018+realAdif@users.noreply.github.com>
2024-01-16 18:53:14 +1100

    Refactor RuleScreen component to support different screen sizes

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -11,29 +11,46 @@ export default function Rules() {
   return (
     <div className="container mx-auto flex lg:justify-end justify-center mb-12">
       <button
-        className="text-white  text-center border px-12 py-3 rounded-md hover:bg-white hover:text-black"
+        className="text-white text-center border px-12 py-3 rounded-md hover:bg-white hover:text-black"
         onClick={() => handleRule()}
       >
         RULES
       </button>
-      {openRule && <RuleScreen onClose={handleRule} />}
+      {openRule && <RuleScreenSm onClose={handleRule} />}
+      {openRule && <RuleScreenLg onClose={handleRule} />}
     </div>
   );
 }
-RuleScreen.propTypes = {
+RuleScreenSm.propTypes = {
   onClose: PropTypes.func.isRequired,
 };
 
-function RuleScreen({ onClose }) {
+function RuleScreenSm({ onClose }) {
   return (
-    <div className="bg-white absolute top-0 h-full w-full">
+    <div className="bg-white absolute top-0 h-full w-full sm:hidden">
       <div className="flex flex-col justify-between items-center h-full">
-        <h1 className="mt-24 text-3xl text-dark-text">Rules</h1>
+        <h1 className="mt-24 text-3xl text-dark-text">RULES</h1>
         <img src={RuleImage} alt="Rules" className="w-full p-4" />
         <button onClick={onClose}>
           <img src={CloseIcon} alt="Close Icon" className="mb-24" />
         </button>
       </div>
+    </div>
+  );
+}
+RuleScreenLg.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
+function RuleScreenLg({ onClose }) {
+  return (
+    <div className="sm:block absolute hidden top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-lg">
+      <div className="flex justify-between">
+        <h1 className=" text-3xl text-dark-text">RULES</h1>
+        <button onClick={onClose}>
+          <img src={CloseIcon} alt="Close Icon" />
+        </button>
+      </div>
+      <img src={RuleImage} alt="Rules" className="w-full p-4" />
     </div>
   );
 }
